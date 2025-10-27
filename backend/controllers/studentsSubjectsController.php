3145f0e2b7fef6fa62470fe6c332dcc72c8e5623
valentin c <valentincam1822@gmail.com>
2025-10-27 23:18:04 +0000

    agregue validacion en subjectscrontroller.js y en subjectscintroller.php .(queda señalizado con 3.0)

--- backend/controllers/studentsSubjectsController.php
+++ backend/controllers/studentsSubjectsController.php
@@ -40,7 +40,32 @@ function handleGet($conn)
     }
 }
 
-function handlePost($conn) 
+function handlePost($conn) //3.0 new
+{
+    $input = json_decode(file_get_contents("php://input"), true);
+
+    try {
+        $result = createSubject($conn, $input['name']);
+        if ($result['inserted'] > 0) {
+            echo json_encode(["message" => "Materia creada correctamente"]);
+        } else {
+            http_response_code(400);
+            echo json_encode(["error" => "No se pudo crear la materia"]);
+        }
+    } catch (mysqli_sql_exception $e) {
+        // 🔹 Código 1062 = duplicado
+        if ($e->getCode() == 1062) {
+            http_response_code(400);
+            echo json_encode(["error" => "La materia ya existe."]);
+        } else {
+            http_response_code(500);
+            echo json_encode(["error" => "Error interno del servidor"]);
+        }
+    }
+}
+
+
+/*function handlePost($conn) //old 
 {
     $input = json_decode(file_get_contents("php://input"), true);
     
@@ -54,7 +79,7 @@ function handlePost($conn)
         http_response_code(500);
         echo json_encode(["error" => "Error al asignar"]);
     }
-}
+}*/
 
 function handlePut($conn) 
 {
